eee7e6d84146bede0fc5b1fdf586a22e00f39910
Juanse2296 <juan-2296@hotmail.es>
2017-05-08 23:51:25 -0500

    Agregar clase message

---
+++ src/front/Message.java
@@ -0,0 +1,34 @@
+package front;
+
+import back.Time;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Message {
+
+	private PImage img[];
+	private Time t;
+	private boolean display = true;
+
+	public Message(PApplet app) {
+		t = new Time();
+		t.Count(1);
+		img = new PImage[2];
+		img[0] = app.loadImage("data/prueba.png");
+		img[1] = app.loadImage("data/intentos.png");
+	}
+
+	public void show(PApplet app, int n) {
+		if (display)
+			app.image(img[n], 0, 0);
+		if (t.getSeconds() > 3 && display) {
+			display = false;
+		}
+	}
+	
+	public void next(){
+		t.setSeconds(0);
+		display=true;		
+	}
+
+}
